e6e9101e7e15831e63747634e26f4c9857168cc4
Joy Osayi <osayijoy16@gmail.com>
2023-04-27 04:00:35 +0100

    Adire frontend

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import 'normalize.css';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { store } from './store';
+import { Provider } from 'react-redux';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="178688632571-jmhkftmvgd028u2frl7ehs7jvqempa0v.apps.googleusercontent.com">
+  {/* // <React.StrictMode> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
+  {/* // </React.StrictMode> */}
+  </GoogleOAuthProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
